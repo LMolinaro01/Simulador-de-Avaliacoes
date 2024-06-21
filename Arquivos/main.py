fac0dac1efd513333ac8aea80d1a1163a71036b2
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-06-21 14:21:00 -0300

    Remoção de Comentários

--- Arquivos/main.py
+++ Arquivos/main.py
@@ -39,14 +39,9 @@ inserir_questao("A área de um triângulo que possui 12 cm de altura e base medi
 connection.commit()
 
 def center(win):
-    # :param win: the main window or Toplevel window to center
+   
+    win.update_idletasks()
 
-    # Apparently a common hack to get the window size. Temporarily hide the
-    # window to avoid update_idletasks() drawing the window in the wrong
-    # position.
-    win.update_idletasks()  # Update "requested size" from geometry manager
-
-    # define window dimensions width and height
     width = win.winfo_width()
     frm_width = win.winfo_rootx() - win.winfo_x()
     win_width = width + 2 * frm_width
@@ -55,15 +50,10 @@ def center(win):
     titlebar_height = win.winfo_rooty() - win.winfo_y()
     win_height = height + titlebar_height + frm_width
 
-    # Get the window position from the top dynamically as well as position from left or right as follows
     x = win.winfo_screenwidth() // 2 - win_width // 2
     y = win.winfo_screenheight() // 2 - win_height // 2
 
-    # this is the line that will center your window
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-
-    # This seems to draw the window frame immediately, so only call deiconify()
-    # after setting correct window position
     win.deiconify()
 
 # TELA INICIAL
